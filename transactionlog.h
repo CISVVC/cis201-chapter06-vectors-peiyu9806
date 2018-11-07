5be1aa4284c3501701ca6cdff793cb5578d6a203
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-06 19:11:50 -0800

    Update transactionlog.h

--- transactionlog.h
+++ transactionlog.h
@@ -1,1 +1,22 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
 
+#include<vector>
+
+using namespace std;
+
+class Transactionlog
+{
+    vector<Transaction> m_transactions;
+    double m_balance;
+    double m_min_balance;
+    double m_average_balance;
+
+public:
+    void add_transaction(const Transaction &t);
+    int min_daily_balance();
+    int average_daily_balance();
+    void print();
+};
+
+#endif
