646eb1e2a5a4d12d18cf86c040a815da21ad605f
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-06 19:18:05 -0800

    Update transactionlog.cpp

--- transactionlog.cpp
+++ transactionlog.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include "transactionlog.h"
 
+using namespace std;
+
+void Transactionlog::add_transaction(const Transaction &t)
+{
+
+}
+
+int Transactionlog::min_daily_balance()
+{
+
+}
+
+int Transactionlog::average_daily_balance()
+{
+
+}
+
+void Transactionlog::print()
+{
+
+}
