c333b56ab7dd3ec3d60a916aba157d77afb28682
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-06 19:05:36 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-// Add your code here.
+/**
+File: main.cpp
+Description: The program will read in the descriptions and then print out a bank statement listing all deposits, withdrawals, and the daily balance for each day.
+Author: Pei-Yu Kuo
+Email: peiyuk9810@student.vvc.edu
+Data Created: 11-06-2018
+**/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main()
+{
+
+    
+    return 0;
+}
+  
