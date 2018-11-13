37079ac212c18ab71cfbda11891d56782e002509
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-12 22:00:36 -0800

    Update

--- transaction.cpp
+++ transaction.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "transaction.h"
 
-using namespace std;
+Transaction::Transaction()
+{
+   day = 0;
+   amount = 0.0;
+}
 
+void Transaction::read()
+{
+    std::cin >> day;
+    std::cin >> amount;
+    std::string remainder;
+    std::getline(cin,remainder);
+    std::getline(std::cin,description);
+}
 
+int Transaction::get_day()
+{
+
+}
+
+double Transaction::get_amount()
+{
+
+}
+
+void Transaction::print() const
+{
+    std::cout << "day:" << day << std::endl;
+    std::cout << "amount:" << amount << std::endl;
+    std::cout << "description:" << description << std::endl;
+}
+
+void Transasction::print_transactions(const std::vector<Transaction> t)
+{
+    for(int i=0;i<t.size();i++)
+        t[i].print();
+}
