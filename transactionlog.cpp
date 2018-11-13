2df6e8a9bd8092d0f1ec6d0f15520ee0c1abb4d3
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-12 23:54:30 -0800

    Update transactionlog.cpp

--- transactionlog.cpp
+++ transactionlog.cpp
@@ -6,20 +6,40 @@ void Transactionlog::add_transaction(const Transaction &t)
     m_transactions.push_back(t);
 }
 
-void Transactionlog::compute_balances()
+void Transactionlog::read()
 {
+    std::cin.ignore();
+    cin >> m_amount;
+    std::cin.ignore();
 }
 
-double Transactionlog::get_average_daily_balance()
+double Transactionlog::compute_balances()
 {
-    
+    for(int i=0; i<daily_balances.size(); i++)
+    {
+        m_total_balance += daily_balances[i];
+    }
+    return m_total_balance;
 }
 
-double Transactionlog::get_minimum_daily_balance()
+double Transactionlog::min_daily_balance()
 {
+    m_min_balance = 500.00;
+    return m_min_balance;
+}
+
+double Transactionlog::average_daily_balance()
+{
+    m_average_balance = m_total_balance / MAX_DAY;
+    return m_average_balance;
+}
+
+void Transactionlog::earned_interest()
+{
+    m_interest = m_total_balance * (1 + INTEREST);
 }
 
 void Transactionlog::print()
 {
-
+    
 }
