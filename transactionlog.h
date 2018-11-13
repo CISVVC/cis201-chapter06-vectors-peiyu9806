51e8a41e692ca6e970bcb48dc36dcf4165284aaf
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-12 22:03:19 -0800

    Update

--- transactionlog.h
+++ transactionlog.h
@@ -4,20 +4,18 @@
 #include<vector>
 #include "transaction.h"
 
-using namespace std;
-
 class Transactionlog
 {
-    vector<Transaction> m_transactions;
-    double m_balance;
-    double m_min_balance;
-    double m_average_balance;
-
 public:
     void add_transaction(const Transaction &t);
     int min_daily_balance();
     int average_daily_balance();
     void print();
+private:
+    std::vector<Transaction> m_transactions;
+    double m_balance;
+    double m_min_balance;
+    double m_average_balance;
 };
 
 #endif
