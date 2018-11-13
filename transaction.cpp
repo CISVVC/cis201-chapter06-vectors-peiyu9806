e3ac9828baef208b1dc5f4dd8be51baa340f3574
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-12 22:40:19 -0800

    Update transaction.cpp

--- transaction.cpp
+++ transaction.cpp
@@ -14,7 +14,7 @@ void Transaction::read()
     std::cin >> day;
     std::cin >> amount;
     std::string remainder;
-    std::getline(cin,remainder);
+    std::cin.ignore();
     std::getline(std::cin,description);
 }
 
@@ -23,10 +23,4 @@ void Transaction::print() const
     std::cout << "day:" << day << std::endl;
     std::cout << "amount:" << amount << std::endl;
     std::cout << "description:" << description << std::endl;
-}
-
-void print_transactions(const std::vector<Transaction> t)
-{
-    for(int i=0;i<t.size();i++)
-        t[i].print();
 }
