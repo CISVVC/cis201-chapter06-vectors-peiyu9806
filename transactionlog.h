0064c23719b332ea0cace4f5d75059f3678539c3
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-12 23:54:29 -0800

    Update transactionlog.h

--- transactionlog.h
+++ transactionlog.h
@@ -8,15 +8,23 @@ class Transactionlog
 {
 public:
     void add_transaction(const Transaction &t);
-    void compute_balances();
-    double get_average_daily_balance();
-    double get_minimum_daily_balance();
+    void read();
+    double compute_balances();
+    double min_daily_balance();
+    double average_daily_balance();
+    void earned_interest();
     void print();
 private:
     std::vector<Transaction> m_transactions;
+    std::vector<double> daily_balances;
+    double m_amount;
     double m_balance;
     double m_min_balance;
     double m_average_balance;
+    double m_total_balance;
+    double m_interest;
+    const int MAX_DAY = 30;
+    const double INTEREST = 0.05;
 };
 
 #endif
