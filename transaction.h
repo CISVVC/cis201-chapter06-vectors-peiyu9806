82ffad9b4ee34ea84604467a445bffe72e2c0d6a
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-12 21:54:06 -0800

    Update

--- transaction.h
+++ transaction.h
@@ -1,15 +1,52 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
-using namespace std;
 
 class Transaction
 {
-    
 public:
-
+   Transaction();
+   void read();
+   int get_day();
+   double get_amount();
+   void print() const;
+   void print_transactions(const std::vector<Transaction> t);
+private:
+   int day;
+   double amount;
+   std::string description;
 };
+
+Transaction::Transaction()
+{
+   day = 0;
+   amount = 0.0;
+}
+
+void Transaction::read()
+{
+    std::cin >> day;
+    std::cin >> amount;
+    std::string remainder;
+    std::getline(cin,remainder);
+    std::getline(std::cin,description);
+}
+
+void Transaction::print() const
+{
+    std::cout << "day:" << day << std::endl;
+    std::cout << "amount:" << amount << std::endl;
+    std::cout << "description:" << description << std::endl;
+}
+
+void Transasction::print_transactions(const std::vector<Transaction> t)
+{
+    for(int i=0;i<t.size();i++)
+        t[i].print();
+}
 
 #endif
