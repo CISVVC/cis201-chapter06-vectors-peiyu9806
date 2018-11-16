6484dd6d095dfe015e9c34d4fbcfbef1c19ceda6
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-11-16 04:52:31 -0800

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -2,18 +2,21 @@
 #define TRANSACTION_H
 
 #include <string>
-#include <vector>
 
 class Transaction
 {
 public:
    Transaction();
+   Transaction(int m_day,  double m_amount, std::string m_description);
    void read();
+   int get_day() const;
+   double get_amount() const;
+   std::string get_descri() const;
    void print() const;
 private:
-   int day;
-   double amount;
-   std::string description;
+   int day, m_day;
+   double amount, m_amount;
+   std::string description, m_description;
 };
 
 #endif
